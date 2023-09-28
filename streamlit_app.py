babdd15586c6126574d42b647c8421b36b1f3cc4
freeEDUfi <109695318+freeEDUfi@users.noreply.github.com>
2023-09-28 14:27:22 +0800

    implement uploading multiple files

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,14 @@ import streamlit as st
 import pandas as pd
 import os
 import openai
+import nltk  # Add this import
 from openai.embeddings_utils import get_embedding, distances_from_embeddings
 import convert_file_to_embeddings as embeddings_converter
 
-#openai API key
+# Add the following lines to download the 'punkt' resource
+nltk.download('punkt')
+
+# openai API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # App information and setup
@@ -37,40 +41,56 @@ if "file_embeddings" not in st.session_state:
 if "text_embeddings" not in st.session_state:
     st.session_state.text_embeddings = None
 
-def create_embeddings_context(question, df, max_len=1800, size="ada"):
-    """Create a context for a question by finding the most similar context from the dataframe"""
+
+def create_embeddings_context(question, embeddings_list, max_len=1800, size="ada"):
+    """Create a context for a question by finding the most similar context from the list of embeddings"""
     # Convert question to embeddings
     q_embeddings = get_embedding(question, engine='text-embedding-ada-002')
-   
-    # Get the distances from the embeddings
-    df['distances'] = distances_from_embeddings(q_embeddings, df['embeddings'].values, distance_metric='cosine')
 
     returns = []
     cur_len = 0
 
     # Sort by distance and add the text to the context until the context is too long
-    for i, row in df.sort_values('distances', ascending=True).iterrows():
-        # Add the length of the text to the current length
-        cur_len += row['n_tokens'] + 4
-        # If the context is too long, break
-        if cur_len > max_len:
-            break
-        # Else add it to the text that is being returned
-        returns.append(row["text"])
+    for file_embeddings in embeddings_list:
+        # Get the distances from the embeddings
+        file_embeddings['distances'] = distances_from_embeddings(q_embeddings, file_embeddings['embeddings'].values,
+                                                                 distance_metric='cosine')
+
+        for i, row in file_embeddings.sort_values('distances', ascending=True).iterrows():
+            # Add the length of the text to the current length
+            cur_len += row['n_tokens'] + 4
+            # If the context is too long, break
+            if cur_len > max_len:
+                break
+            # Else add it to the text that is being returned
+            returns.append(row["text"])
 
     # Return the context
     return "\n\n###\n\n".join(returns)
 
+
 def convert_inputs_to_embeddings():
-    """Converts uploaded file and text area input into embeddings"""
+    """Converts uploaded files and text area input into embeddings"""
+    st.session_state.file_embeddings = []  # Initialize a list to store embeddings for multiple files
+
     if st.session_state.uploaded_file is not None:
-        if st.session_state.uploaded_file.type == "text/plain":
-            st.session_state.file_embeddings = embeddings_converter.generate_embeddings(openai.api_key, st.session_state.uploaded_file, is_text_file=True)
-        elif st.session_state.uploaded_file.type == "application/pdf":
-            st.session_state.file_embeddings = embeddings_converter.generate_embeddings(openai.api_key, st.session_state.uploaded_file, is_pdf_file=True)
+        for uploaded_file in st.session_state.uploaded_file:
+            if uploaded_file.type == "text/plain":
+                file_embeddings = embeddings_converter.generate_embeddings(openai.api_key, uploaded_file, is_text_file=True)
+                st.session_state.file_embeddings.append(file_embeddings)
+                print(f"Text File Embeddings ({uploaded_file.name}):")
+                print(file_embeddings)  # Print the file embeddings
+            elif uploaded_file.type == "application/pdf":
+                file_embeddings = embeddings_converter.generate_embeddings(openai.api_key, uploaded_file, is_pdf_file=True)
+                st.session_state.file_embeddings.append(file_embeddings)
+                print(f"PDF File Embeddings ({uploaded_file.name}):")
+                print(file_embeddings)  # Print the file embeddings
 
     if st.session_state.custom_data_text != "":
-        st.session_state.text_embeddings = embeddings_converter.generate_embeddings(openai.api_key, st.session_state.custom_data_text)
+        st.session_state.text_embeddings = embeddings_converter.generate_embeddings(openai.api_key,
+                                                                                    st.session_state.custom_data_text)
+        print("Text Area Embeddings:")
+        print(st.session_state.text_embeddings)  # Print the text area embeddings
 
 def get_context():
     file_embeddings_context = ""
@@ -79,12 +99,12 @@ def get_context():
 
     if st.session_state.use_custom_data:
         if st.session_state.file_embeddings is not None:
-            file_embeddings_context = create_embeddings_context(question=last_message, 
-                                                                df=st.session_state.file_embeddings, 
+            file_embeddings_context = create_embeddings_context(question=last_message,
+                                                                embeddings_list=st.session_state.file_embeddings,
                                                                 max_len=900)
         if st.session_state.text_embeddings is not None:
             text_embeddings_context = create_embeddings_context(question=last_message,
-                                                                df=st.session_state.text_embeddings,
+                                                                embeddings_list=st.session_state.text_embeddings,
                                                                 max_len=900)
 
     context = f"""
@@ -98,8 +118,10 @@ Refrain from responding to queries that violate standard moderation policies. Us
 
     return context
 
+
+
 def main():
-    head_col = st.columns([1,8])
+    head_col = st.columns([1, 8])
     with head_col[0]:
         st.image(project_icon)
     with head_col[1]:
@@ -110,16 +132,18 @@ def main():
     reset_chat = st.button("Reset Chat")
     if reset_chat:
         st.session_state.messages = []
-        st.success("Chat has been reset.")        
+        st.success("Chat has been reset.")
     st.markdown("***")
-#########################################
+    #########################################
     with st.sidebar:
         st.session_state.model = st.selectbox("Select a model", options=["gpt-4", "gpt-3.5-turbo"], index=0)
         st.session_state.use_custom_data = st.checkbox("Chat with Custom Data")
         if st.session_state.use_custom_data:
-            st.session_state.uploaded_file = st.file_uploader("Upload a .pdf or .txt file", type=["pdf", "txt"], accept_multiple_files=False)
+            st.session_state.uploaded_file = st.file_uploader("Upload a .pdf or .txt file", type=["pdf", "txt"],
+                                                              accept_multiple_files=True)
             st.session_state.custom_data_text = st.text_area("Or copy-paste your data here", height=200)
-            st.write("**Note:** OpenAI API has a request limit; if your custom data is large, you may have to wait a while for the embeddings to be generated.")
+            st.write(
+                "**Note:** OpenAI API has a request limit; if your custom data is large, you may have to wait a while for the embeddings to be generated.")
             if st.button("Generate Embeddings"):
                 with st.spinner("Generating embeddings..."):
                     convert_inputs_to_embeddings()
@@ -130,10 +154,10 @@ def main():
                 st.session_state.file_embeddings = None
                 st.session_state.text_embeddings = None
                 st.success("Embeddings have been reset.")
-            
+
     for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
     # Accept user input
     if prompt := st.chat_input(placeholder="Type a message..."):
@@ -150,19 +174,20 @@ def main():
 
             # Build messages to be sent to ChatGPT
             context = get_context()
-            messages = [{"role":"system", "content":context}]
+            messages = [{"role": "system", "content": context}]
             # Add the last 20 messages in the conversation to the messages list
             messages.extend(st.session_state.messages[-20:])
             # Generate response from ChatGPT
             for response in openai.ChatCompletion.create(
-                                                    model=st.session_state.model,
-                                                    messages=messages,
-                                                    stream=True,
-                                                ):
+                    model=st.session_state.model,
+                    messages=messages,
+                    stream=True,
+            ):
                 full_response += response.choices[0].delta.get("content", "")
                 message_placeholder.markdown(full_response + "▌")
             message_placeholder.markdown(full_response)
         st.session_state.messages.append({"role": "assistant", "content": full_response})
+
 
 if __name__ == "__main__":
     main()
